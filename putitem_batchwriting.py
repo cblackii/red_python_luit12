7926d6276a1b38e895345ed2ccac3f9ae0479e85
John Doe <johndoe@email.com>
2023-02-18 03:05:03 +0000

    modified putitem

--- putitem_batchwriting.py
+++ putitem_batchwriting.py
@@ -11,61 +11,62 @@ table = dynamodb.Table('NBAs_All_Time_Leading_Scorers') #variable to hold table 
 with table.batch_writer() as batch: #batch writing 10 items
     batch.put_item(
         Item={
-            'Player': 'Lebron_James',
-            'Points': '38411',
+            "Player": 'Lebron_James',
+            "Points": '38411',
             }
     )
     batch.put_item(
         Item={
-            'Player': 'Kareem_Abdual_Jabbar',
-            'Points': '38337',
+            "Player": 'Kareem_Abdual_Jabbar',
+            "Points": '38337',
             }
     )
     batch.put_item(
         Item={
-            'Player': 'Karl_Malone',
-            'Points': '36928',
+            "Player": 'Karl_Malone',
+            "Points": '36928',
             }
     )
     batch.put_item(
         Item={
-            'Player': 'Kobe_Bryant',
-            'Points': '33643',
+            "Player": 'Kobe_Bryant',
+            "Points": '33643',
             }
     )
     batch.put_item(
         Item={
-            'Player': 'Michael_Jordan',
-            'Points': '32292',
+            "Player": 'Michael_Jordan',
+            "Points": '32292',
             }
     )
     batch.put_item(
         Item={
-            'Player': 'Dirk_Nowitzki',
-            'Points': '31560',
+            "Player": 'Dirk_Nowitzki',
+            "Points": '31560',
             }
     )
     batch.put_item(
         Item={
-            'Player': 'Wilt_Chamberlain',
-            'Points': '31419',
+            "Player": 'Wilt_Chamberlain',
+            "Points": '31419',
             }
     )
     batch.put_item(
         Item={
-            'Player': 'Shaquille_Oneal',
-            'Points': '28596',
+            "Player": 'Shaquille_Oneal',
+            "Points": '28596',
             }
     )
     batch.put_item(
         Item={
-            'Player': 'Carmelo_Anthony',
-            'Points': '28289',
+            "Player": 'Carmelo_Anthony',
+            "Points": '28289',
             }
     )
     batch.put_item(
         Item={
-            'Player': 'Moses_Malone',
-            'Points': '27409',
+            "Player": 'Moses_Malone',
+            "Points": '27409',
             }
     )
+    print(batch)
