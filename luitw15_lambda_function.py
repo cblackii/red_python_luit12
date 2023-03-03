a484e6f498071e77c511ecc4a0b5b91d31ea2334
John Doe <johndoe@email.com>
2023-03-03 05:29:32 +0000

    Updated Lambda Functions

--- luitw15_lambda_function.py
+++ luitw15_lambda_function.py
@@ -2,19 +2,18 @@
 
 import json
 import boto3
-from datetime import datetime
-
+import random
 
 def lambda_handler(event, context):
-    # TODO implement
-    now = datetime.now()
-    date_time = now.strftime("%m-%d-%y %H:%M:%S")
     
     sqs = boto3.client('sqs')
-    
+
     response = sqs.send_message(
         QueueUrl='https://sqs.us-west-2.amazonaws.com/542986597955/luitw15-queue',
-        Messagebody=time_date)
+        MessageBody=str(random.randint(0,100000))
+    )
+    
+    # TODO implement
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
